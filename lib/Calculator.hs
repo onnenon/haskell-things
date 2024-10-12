69435e1861f9ffff6e1b2123817268af69117d1c
Stephen Onnen <stephen.onnen@gmail.com>
2024-10-12 10:26:13 -0500

    update docstring

--- lib/Calculator.hs
+++ lib/Calculator.hs
@@ -6,14 +6,15 @@ data Operation = Add | Subtract | Multiply | Divide | OpList [Operation]
 
 -- | A simple math calculator that takes either two Nums and a single Operation or
 -- two Lists of Nums and a List of Operations
--- ==== __Examples__
 --
--- Basic Usage:
+-- __Basic Usage:__
 --
 -- >>> calculate (List [1, 2, 3]) (List [4, 5, 6]) (OpList [Add, Subtract, Multiply])
 -- Right (List [5,-3,18])
 -- >>> calculate (Num 1) (Num 2) Add
 -- Right (Num 3)
+-- >>> calculate (Num 7) (Num 0) Divide
+-- Left "Cannot divide by zero"
 calculate :: DTO -> DTO -> Operation -> Either String DTO
 calculate (Num x) (Num y) Add = Right $ Num (x + y)
 calculate (Num x) (Num y) Subtract = Right $ Num (x - y)
