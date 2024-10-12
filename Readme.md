62262a5163da799797fa3291bcfd6e29370ce900
Stephen Onnen <stephen.onnen@gmail.com>
2024-10-12 10:21:30 -0500

    update docstring

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Haskell Things
+This repo serves as a scratch pad for me to learn Haskell by trying out small things
--- lib/Calculator.hs
+++ lib/Calculator.hs
@@ -4,6 +4,16 @@ data DTO = List [Int] | Num Int deriving (Show)
 
 data Operation = Add | Subtract | Multiply | Divide | OpList [Operation]
 
+-- | A simple math calculator that takes either two Nums and a single Operation or
+-- two Lists of Nums and a List of Operations
+-- ==== __Examples__
+--
+-- Basic Usage:
+--
+-- >>> calculate (List [1, 2, 3]) (List [4, 5, 6]) (OpList [Add, Subtract, Multiply])
+-- Right (List [5,-3,18])
+-- >>> calculate (Num 1) (Num 2) Add
+-- Right (Num 3)
 calculate :: DTO -> DTO -> Operation -> Either String DTO
 calculate (Num x) (Num y) Add = Right $ Num (x + y)
 calculate (Num x) (Num y) Subtract = Right $ Num (x - y)
@@ -19,6 +29,3 @@ calculatePair (a, b, op) = case calculate (Num a) (Num b) op of
   Right (Num result) -> Right result
   Right (List _) -> Left "Unexpected List result"
   Left err -> Left err
-
--- >>> List <$> traverse calculatePair [(1, 2, Add), (3, 4, Subtract), (5, 6, Multiply), (7, 1, Divide)]
--- Right (List [3,-1,30,7])
